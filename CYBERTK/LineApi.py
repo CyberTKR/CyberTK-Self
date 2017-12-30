9c2df6500d28ede87c58c6580a3fe8718feb77b6
TKR <Cybertk7@gmail.com>
2017-12-30 14:34:38 +0200

    Add files via upload

---
+++ CYBERTK/LineApi.py
@@ -0,0 +1,543 @@
+# -*- coding: utf-8 -*-
+import requests,shutil,random,string,json,os,tempfile
+import unicodedata
+from Api import Poll, Talk, channel
+from random import randint
+from time import time
+from datetime import datetime
+from lib.curve.ttypes import *
+
+def def_callback(str):
+    print(str)
+
+class LINE:
+
+  mid = None
+  authToken = None
+  cert = None
+  channel_access_token = None
+  token = None
+  obs_token = None
+  refresh_token = None
+
+
+  def __init__(self):
+    self.Talk = Talk()
+    self._session = requests.session()
+    self._headers = {'X-Line-Application': 'IOSIPAD\t7.14.0\tiPhone OS\t10.12.0', 'X-Line-Access': 'Emp1jl3qOjxCjXEhmaN5.QdLXoVPaKOU6WpvD80Sijq.NcwnmLOaI/dIyi3Y84WTCOxbNTN27m3ODDpkMLDPY64=', 'User-Agent': 'Line/7.14.0'}
+
+  def login(self, mail=None, passwd=None, cert=None, token=None, qr=False, callback=None):
+    if callback is None:
+      callback = def_callback
+    resp = self.__validate(mail,passwd,cert,token,qr)
+    if resp == 1:
+      self.Talk.login(mail, passwd, callback=callback)
+    elif resp == 2:
+      self.Talk.login(mail,passwd,cert, callback=callback)
+    elif resp == 3:
+      self.Talk.TokenLogin(token)
+    elif resp == 4:
+      self.Talk.qrLogin(callback)
+    else:
+      raise Exception("invalid arguments")
+
+    self.authToken = self.Talk.authToken
+    self.cert = self.Talk.cert
+    self._headers = {
+              'X-Line-Application': 'IOSIPAD\t7.14.0\tiPhone OS\t10.12.0', 
+              'X-Line-Access': self.authToken, 
+              'User-Agent': 'Line/7.14.0'
+   }
+    self.Poll = Poll(self.authToken)
+    self.channel = channel.Channel(self.authToken)
+    self.channel.login()	
+    self.mid = self.channel.mid
+    self.channel_access_token = self.channel.channel_access_token
+    self.token = self.channel.token
+    self.obs_token = self.channel.obs_token
+    self.refresh_token = self.channel.refresh_token
+
+
+  """User"""
+
+  def getProfile(self):
+    return self.Talk.client.getProfile()
+
+  def getSettings(self):
+    return self.Talk.client.getSettings()
+
+  def getUserTicket(self):
+    return self.Talk.client.getUserTicket()
+
+  def updateProfile(self, profileObject):
+    return self.Talk.client.updateProfile(0, profileObject)
+
+  def updateSettings(self, settingObject):
+    return self.Talk.client.updateSettings(0, settingObject)
+
+  def CloneContactProfile(self, mid):
+    contact = self.getContact(mid)
+    profile = self.getProfile()
+    profile.displayName = contact.displayName
+    profile.statusMessage = contact.statusMessage
+    profile.pictureStatus = contact.pictureStatus
+    self.updateDisplayPicture(profile.pictureStatus)
+    return self.updateProfile(profile)
+
+  def updateDisplayPicture(self, hash_id):
+    return self.Talk.client.updateProfileAttribute(0, 8, hash_id)
+
+  def updateProfilePicture(self, path):
+        file=open(path, 'rb')
+        files = {
+            'file': file
+        }
+        params = {
+            'name': 'media',
+            'type': 'image',
+            'oid': self.profile.mid,
+            'ver': '1.0',
+        }
+        data={
+            'params': json.dumps(params)
+        }
+        r = self.server.postContent(self.server.LINE_OBS_DOMAIN + '/talk/p/upload.nhn', data=data, files=files)
+        if r.status_code != 201:
+            raise Exception('Update profile picture failure.')
+        return True
+
+  """Operation"""
+
+  def fetchOperation(self, revision, count):
+        return self.Poll.client.fetchOperations(revision, count)
+
+  def fetchOps(self, rev, count):
+        return self.Poll.client.fetchOps(rev, count, 0, 0)
+
+  def getLastOpRevision(self):
+        return self.Talk.client.getLastOpRevision()
+
+  def stream(self):
+        return self.Poll.stream()
+
+  """Message"""
+
+  def sendMessage(self, messageObject):
+        return self.Talk.client.sendMessage(0,messageObject)
+
+  def removeAllMessage(self, messageObject):
+        return self.Talk.client.removeAllMessage(0,lastMessageId)
+
+  def sendText(self, Tomid, text):
+        msg = Message()
+        msg.to = Tomid
+        msg.text = text
+
+        return self.Talk.client.sendMessage(0, msg)
+  def post_content(self, url, data=None, files=None):
+        return self._session.post(url, headers=self._headers, data=data, files=files)
+
+  def sendImage(self, to_, path):
+        M = Message(to=to_, text=None, contentType = 1)
+        M.contentMetadata = None
+        M.contentPreview = None
+        M_id = self.Talk.client.sendMessage(0,M).id
+        files = {
+            'file': open(path, 'rb'),
+        }
+        params = {
+            'name': 'media',
+            'oid': M_id,
+            'size': len(open(path, 'rb').read()),
+            'type': 'image',
+            'ver': '1.0',
+        }
+        data = {
+            'params': json.dumps(params)            
+        }       
+
+        r = self.post_content('https://os.line.naver.jp/talk/m/upload.nhn', data=data, files=files)
+        #print r
+        if r.status_code != 201:
+            raise Exception('Upload image failure.')
+        return True
+
+  def sendImageWithUrl(self, to_, url):
+        """Send a image with given image url
+
+        :param url: image url to send
+        """
+        path = '%s/pythonLine-%1.data' % (tempfile.gettempdir(), randint(0, 9))
+
+
+        r = requests.get(url, stream=True)
+        if r.status_code == 200:
+            with open(path, 'w') as f:
+                shutil.copyfileobj(r.raw, f)
+        else:
+            raise Exception('Download image failure.')
+
+        try:
+            self.sendImage(to_, path)
+        except Exception as e:
+            raise e
+
+  def sendVideo(self, to_, path):
+        M = Message(to=to_, text=None, contentType = 2)
+        M.contentMetadata = None
+        M.contentPreview = None
+        M2 = self.Talk.client.sendMessage(0,M)
+        M_id = M2.id
+        files = {
+            'file': open(path, 'rb'),
+        }
+        params = {
+            'name': 'media',
+            'oid': M_id,
+            'size': len(open(path, 'rb').read()),
+            'type': 'video',
+            'ver': '1.0',
+        }
+        data = {
+            'params': json.dumps(params)
+        }
+        r = self.post_content('https://os.line.naver.jp/talk/m/upload.nhn', data=data, files=files)
+        #print r
+        if r.status_code != 201:
+            raise Exception('Upload video failure.')
+        return True
+
+  def sendVideoWithURL(self, to_, url):
+        path = '%s/pythonLine-%i.data' % (tempfile.gettempdir(), randint(0, 9))
+
+        r = requests.get(url, stream=True)
+        if r.status_code == 200:
+            with open(path, 'w') as f:
+                shutil.copyfileobj(r.raw, f)
+        else:
+            raise Exception('Download video failure.')
+
+        try:
+            self.sendVideo(to_, path)
+        except Exception as e:
+            raise (e)
+
+  def sendAudio(self, to_, path):
+        M = Message(to=to_, text=None, contentType = 3)
+        M.contentMetadata = None
+        M.contentPreview = None
+        M2 = self.Talk.client.sendMessage(0,M)
+        M_id = M2.id
+        files = {
+            'file': open(path, 'rb'),
+        }
+        params = {
+            'name': 'media',
+            'oid': M_id,
+            'size': len(open(path, 'rb').read()),
+            'type': 'audio',
+            'ver': '1.0',
+        }
+        data = {
+            'params': json.dumps(params)
+        }
+        r = self.post_content('https://os.line.naver.jp/talk/m/upload.nhn', data=data, files=files)
+        if r.status_code != 201:
+            raise Exception('Upload audio failure.')
+        return True
+
+  def sendAudioWithUrl(self, to_, url):
+        path = '%s/pythonLine-%1.data' % (tempfile.gettempdir(), randint(0, 9))
+
+        r = requests.get(url, stream=True)
+        if r.status_code == 200:
+            with open(path, 'w') as f:
+                shutil.copyfileobj(r.raw, f)
+        else:
+            raise Exception('Download audio failure.')
+
+        try:
+            self.sendAudio(to_, path)
+        except Exception as e:
+            raise (e)
+
+  def removeAllMessages(self, lastMessageId):
+	return self.Talk.client.removeAllMessages(0, lastMessageId)
+
+  def sendEvent(self, messageObject):
+        return self._client.sendEvent(0, messageObject)
+
+  def sendChatChecked(self, mid, lastMessageId):
+        return self.Talk.client.sendChatChecked(0, mid, lastMessageId)
+
+  def getMessageBoxCompactWrapUp(self, mid):
+        return self.Talk.client.getMessageBoxCompactWrapUp(mid)
+
+  def getMessageBoxCompactWrapUpList(self, start, messageBox):
+        return self.Talk.client.getMessageBoxCompactWrapUpList(start, messageBox)
+
+  def getRecentMessages(self, messageBox, count):
+        return self.Talk.client.getRecentMessages(messageBox.id, count)
+
+  def getMessageBox(self, channelId, messageboxId, lastMessagesCount):
+        return self.Talk.client.getMessageBox(channelId, messageboxId, lastMessagesCount)
+
+  def getMessageBoxList(self, channelId, lastMessagesCount):
+        return self.Talk.client.getMessageBoxList(channelId, lastMessagesCount)
+
+  def getMessageBoxListByStatus(self, channelId, lastMessagesCount, status):
+        return self.Talk.client.getMessageBoxListByStatus(channelId, lastMessagesCount, status)
+
+  def getMessageBoxWrapUp(self, mid):
+        return self.Talk.client.getMessageBoxWrapUp(mid)
+
+  def getMessageBoxWrapUpList(self, start, messageBoxCount):
+        return self.Talk.client.getMessageBoxWrapUpList(start, messageBoxCount)
+
+  """Contact"""
+
+
+  def blockContact(self, mid):
+        return self.Talk.client.blockContact(0, mid)
+
+
+  def unblockContact(self, mid):
+        return self.Talk.client.unblockContact(0, mid)
+
+
+  def findAndAddContactsByMid(self, mid):
+        return self.Talk.client.findAndAddContactsByMid(0, mid)
+
+
+  def findAndAddContactsByMids(self, midlist):
+        for i in midlist:
+            self.Talk.client.findAndAddContactsByMid(0, i)
+
+  def findAndAddContactsByUserid(self, userid):
+        return self.Talk.client.findAndAddContactsByUserid(0, userid)
+
+  def findContactsByUserid(self, userid):
+        return self.Talk.client.findContactByUserid(userid)
+
+  def findContactByTicket(self, ticketId):
+        return self.Talk.client.findContactByUserTicket(ticketId)
+
+  def getAllContactIds(self):
+        return self.Talk.client.getAllContactIds()
+
+  def getBlockedContactIds(self):
+        return self.Talk.client.getBlockedContactIds()
+
+  def getContact(self, mid):
+        return self.Talk.client.getContact(mid)
+
+  def getContacts(self, midlist):
+        return self.Talk.client.getContacts(midlist)
+
+  def getFavoriteMids(self):
+        return self.Talk.client.getFavoriteMids()
+
+  def getHiddenContactMids(self):
+        return self.Talk.client.getHiddenContactMids()
+
+
+  """Group"""
+
+  def findGroupByTicket(self, ticketId):
+        return self.Talk.client.findGroupByTicket(ticketId)
+
+  def acceptGroupInvitation(self, groupId):
+        return self.Talk.client.acceptGroupInvitation(0, groupId)
+
+  def acceptGroupInvitationByTicket(self, groupId, ticketId):
+        return self.Talk.client.acceptGroupInvitationByTicket(0, groupId, ticketId)
+
+  def cancelGroupInvitation(self, groupId, contactIds):
+        return self.Talk.client.cancelGroupInvitation(0, groupId, contactIds)
+
+  def createGroup(self, name, midlist):
+        return self.Talk.client.createGroup(0, name, midlist)
+
+  def getGroup(self, groupId):
+        return self.Talk.client.getGroup(groupId)
+
+  def getGroups(self, groupIds):
+        return self.Talk.client.getGroups(groupIds)
+
+  def getGroupIdsInvited(self):
+        return self.Talk.client.getGroupIdsInvited()
+
+  def getGroupIdsJoined(self):
+        return self.Talk.client.getGroupIdsJoined()
+
+  def inviteIntoGroup(self, groupId, midlist):
+        return self.Talk.client.inviteIntoGroup(0, groupId, midlist)
+
+  def kickoutFromGroup(self, groupId, midlist):
+        return self.Talk.client.kickoutFromGroup(0, groupId, midlist)
+
+  def leaveGroup(self, groupId):
+        return self.Talk.client.leaveGroup(0, groupId)
+
+  def rejectGroupInvitation(self, groupId):
+        return self.Talk.client.rejectGroupInvitation(0, groupId)
+
+  def reissueGroupTicket(self, groupId):
+        return self.Talk.client.reissueGroupTicket(groupId)
+
+  def updateGroup(self, groupObject):
+        return self.Talk.client.updateGroup(0, groupObject)
+  def findGroupByTicket(self,ticketId):
+        return self.Talk.client.findGroupByTicket(0,ticketId)
+
+  """Room"""
+
+  def createRoom(self, midlist):
+    return self.Talk.client.createRoom(0, midlist)
+
+  def getRoom(self, roomId):
+    return self.Talk.client.getRoom(roomId)
+
+  def inviteIntoRoom(self, roomId, midlist):
+    return self.Talk.client.inviteIntoRoom(0, roomId, midlist)
+
+  def leaveRoom(self, roomId):
+    return self.Talk.client.leaveRoom(0, roomId)
+
+  """TIMELINE"""
+
+  def new_post(self, text):
+    return self.channel.new_post(text)
+
+  def like(self, mid, postid, likeType=1001):
+    return self.channel.like(mid, postid, likeType)
+
+  def comment(self, mid, postid, text):
+    return self.channel.comment(mid, postid, text)
+
+  def activity(self, limit=20):
+    return self.channel.activity(limit)
+
+  def getAlbum(self, gid):
+
+      return self.channel.getAlbum(gid)
+  def changeAlbumName(self, gid, name, albumId):
+      return self.channel.changeAlbumName(gid, name, albumId)
+
+  def deleteAlbum(self, gid, albumId):
+      return self.channel.deleteAlbum(gid,albumId)
+
+  def getNote(self,gid, commentLimit, likeLimit):
+      return self.channel.getNote(gid, commentLimit, likeLimit)
+
+  def getDetail(self,mid):
+      return self.channel.getDetail(mid)
+
+  def getHome(self,mid):
+      return self.channel.getHome(mid)
+
+  def createAlbum(self, gid, name):
+      return self.channel.createAlbum(gid,name)
+
+  def createAlbum2(self, gid, name, path):
+      return self.channel.createAlbum(gid, name, path, oid)
+
+
+  def __validate(self, mail, passwd, cert, token, qr):
+    if mail is not None and passwd is not None and cert is None:
+      return 1
+    elif mail is not None and passwd is not None and cert is not None:
+      return 2
+    elif token is not None:
+      return 3
+    elif qr is True:
+      return 4
+    else:
+      return 5
+
+  def loginResult(self, callback=None):
+    if callback is None:
+      callback = def_callback
+
+      prof = self.getProfile()
+
+      print("\n[Ȼyber | τK  в̴̡̛͈̖̺͖̙̝̩̞̐̂̀͂̏̚͟͠σ̊̑̾͘т\n")
+      print("Mid -> " + prof.mid)
+      print("Name -> " + prof.displayName)
+      print("Token -> " + self.authToken)
+      print("Certifica -> " + self.cert if self.cert is not None else "")
+
+
+
+class AniSearch(object):
+  ## REQUIRED : urllib , requests
+  def __init__(self, path):
+    self.pic_path = path
+    self.b64img = None
+    self.season = None; self.anime = None
+  def image_b64(self):
+    with open(self.pic_path, 'rb') as img_file:
+      encode_str = img_file.read()
+      self.b64img = encode_str.encode('base64')
+  def get_info(self, season, anime):
+    js = self.get_info_(season,anime)
+    ret_r = []
+    for item in js:
+      ret = {}
+      ret['name_en'] = item['title_english']
+      ret['name_jp'] = item['title_japanese']
+      ret['genres'] = item['genres']
+      ret['s_type'] = item['series_type']
+      ret_r.append(ret)
+    return ret_r
+  def get_data(self, json):
+    js = json;
+    results = js['docs']
+    ret = []
+    for result in results:
+      try:
+        season = result['season']
+        anime = result['anime']
+        file = result['file']
+        start_ = result['start']
+        end___ = result['end']
+        token = result['token']
+        token_ = result['tokenthumb']
+        t__ = result['to']
+        ncoded_anime = urllib.quote(anime) ; ncoded_file = urllib.quote(file)
+        url_r = 'https://whatanime.ga/' + season + '/' + ncoded_anime + '/' + ncoded_file
+        url_r += '?start={0}&end={1}&token={2}'.format(start_,end___,token)
+        url_t = 'https://whatanime.ga/thumbnail.php?season={0}&anime={1}&file={2}&t={3}&token={4}'.format(season,ncoded_anime,ncoded_file,t__,token_)
+        ret.append({'video':url_r,'thumbnail':url_t,'anime_name':anime,'season':season})
+      except Exception as e:
+        pass
+    return ret
+  def get_info_(self, season, anime):
+    header = {
+      'Referer':'https://whatanime.ga/',
+      'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+      'x-requested-with':'XMLHttpRequest',
+    }
+    url = 'https://whatanime.ga/info?season={0}&anime={1}'.format(season, urllib.quote(anime));
+    r = requests.get(url, headers=header)
+    return r.json()
+  def post_image(self):
+    try:
+      self.image_b64()
+    except Exception as e: raise Exception("Image cant be converted.");
+    payload = "data=data:image/jpeg;base64," + self.b64img
+    payload += '&filter=*&trial=4'
+    header = {
+      'Host':'whatanime.ga',
+      'accept':'application/json, text/javascript, */*; q=0.01',
+      'content-type':'application/x-www-form-urlencoded; charset=UTF-8',
+      'origin':'https://whatanime.ga',
+      'referer':'https://whatanime.ga/',
+      'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+      'x-requested-with':'XMLHttpRequest',
+    }
+    r = requests.post('https://whatanime.ga/search', data=payload, headers=header)
+    if r.status_code == 200:
+      return r.json()
+    else:
+      raise Exception("Post failed.")
